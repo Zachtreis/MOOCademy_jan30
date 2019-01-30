4b02e9fa95b979d1a7fe7a7360d6cb0e21441233
Zachtreis <zacharie.treister@gmail.com>
2019-01-30 22:18:38 +0100

    tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'faker'
+# User.destroy_all
+
+#Create new cours
+# 5.times do
+#   cour = Cour.create!(titre: Faker::Lorem.word, description: Faker::Lorem.paragraph)
+# end
+
+
+
+#Create new lecons
+15.times do
+  lecon = Lecon.create!(titre: Faker::Lorem.word, body: Faker::Lorem.paragraph, cour_id: rand(1..5))
+end
